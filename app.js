f1a0b0b6a6c94a25d57fb9e0b4cdba54247c0509
keleko <mariomarsan7@gmail.com>
2016-12-21 22:01:08 +0100

    Añadir meta tags para SEO y Open Graph
    Add: 
    Tags para SEO con NgMeta
    Angular-youtube-embed para embeber videos
    Añadido opcionalmente comentarios para cada banda en home.detail.html

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var bandsApp = angular.module('bandsApp', ['ui.router']);
+var bandsApp = angular.module('bandsApp', ['ui.router', 'ngMeta', 'youtube-embed']);
 
 bandsApp.config(function($stateProvider, $urlRouterProvider) {
 
@@ -21,31 +21,55 @@ bandsApp.config(function($stateProvider, $urlRouterProvider) {
             abstract: true,
             url: '/home',
             templateUrl: 'views/home.html',
-            controller: 'bandsController'
+            controller: 'bandsController',
+            data: {
+                'meta': {
+                    'autor': 'mario martin'
+                }
+            }
         })
         .state('home.list', {
             url: '/list',
             // loaded into ui-view of parent's template
-            templateUrl: 'views/home.list.html'
+            templateUrl: 'views/home.list.html',
+            // Data inheritance from parent -> Home
+            data: {
+                'meta': {
+                    'title': 'Angular Rocks',
+                    'description': 'Esto es la descripción por defecto',
+                    'keywords': 'angular rocks default',
+                    'og:title': 'Angular XX bands Rocks',
+                    'og:description': 'Visita nuestra página de bandas de rock'
+                }
+            }
         })
         .state('home.detail', {
             url: '/:id',
             // loaded into ui-view of parent's template
             templateUrl: 'views/home.detail.html',
-            controller: function($scope, $stateParams){
-                $scope.band = $scope.bands[$stateParams.id];
-                // console.log($scope.band);
+            controller: 'homeDetailController',
+            // Data inheritance from parent -> Home
+            data: {
+                'meta': {
+                    'title': 'A rock band',
+                    'description': 'Descripción de la banda de rock iría aquí',
+                    'keywords': 'angular rock band',
+                    'og:title': 'Angular the best band',
+                    'og:description': 'Una de las bandas de rock'
+                }
             }
 
         })
-
-
-
 
 });
 
 bandsApp.controller('maintenanceController', function($scope){
     $scope.message = 'En construccion';
+});
+
+bandsApp.controller('homeDetailController', function($scope, $stateParams){
+    $scope.band = $scope.bands[$stateParams.id];
+    // console.log($scope.band);
 });
 
 bandsApp.controller('bandsController', function($scope) {
@@ -57,26 +81,70 @@ bandsApp.controller('bandsController', function($scope) {
             id: 0,
             name: 'The Rolling Stones',
             url: 'the-rolling-stones',
-            embed: 'https://www.youtube.com/embed/iXd6O321Ruc',
+            embed: 'iXd6O321Ruc',
             image: 'images/rolling1.jpg',
+            comments: [
+                {author: 'Lucy Perry',
+                 comment: 'ras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                cimage: 'images/background.jpg'
+                },
+                {author: 'Steven Barry',
+                 comment: 'nibh libero, risque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                cimage: 'images/rock-concert.jpg'
+                },
+                {author: 'Alex Crowel',
+                    comment: 'Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                    cimage: 'images/queen1.jpg'
+                }
+            ],
             text: 'The Rolling Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui'
         },
         {
             id: 1,
             name: 'The Beatles',
             url: 'the-beatles',
-            embed: 'https://www.youtube.com/embed/Jbt8oH5Lxto',
+            embed: 'Jbt8oH5Lxto',
             image: 'images/beatles1.jpg',
+            comments: [
+                {author: 'Jessie L',
+                    comment: 'ravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                    cimage: 'images/rolling1.jpg'
+                },
+                {author: 'Alex Crowel',
+                    comment: 'rlla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                    cimage: 'images/rock-concert.jpg'
+                }
+            ],
             text: 'The Beathes Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
         },
         {
             id: 2,
             name: 'Queen',
             url: 'queen',
-            embed: 'https://www.youtube.com/embed/WUOtCLOXgm8',
+            embed: 'WUOtCLOXgm8',
             image: 'images/queen1.jpg',
+            comments: [
+                {author: 'John Brown',
+                    comment: 'Fd in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                    cimage: 'images/rolling1.jpg'
+                },
+                {author: 'Alexia S.',
+                    comment: 'scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                    cimage: 'images/concert-joy.jpg'
+                },
+                {author: 'Nora Lewis',
+                    comment: 'ras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.',
+                    cimage: 'images/rock-concert.jpg'
+                }
+            ],
             text: 'Queen Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
         }
     ];
 
+});
+
+
+bandsApp.run(function(ngMeta) {
+    // Initialize ngMeta
+    ngMeta.init();
 });
